a2dc94fa8e6ae59de0853f5c224f7984ab30ca04
minhkhoango <ngominhkhoa2006@gmail.com>
2025-03-02 09:51:30 -0500

    MEH

--- spam_email/spam/views.py
+++ spam_email/spam/views.py
@@ -1,43 +1,39 @@
 from django.shortcuts import render
-
+from django.conf import settings
+import os
 # Create your views here.
-import pandas as pd
 import joblib
 from nltk import word_tokenize
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
 
-model_name = 'model.joblib'
-vectorizer_name = 'vectorizer.joblib'
 
-def main():
-    model, vectorizer = load_models()
-
-    X_pred = get_input(vectorizer)
-    y_pred = model.predict(X_pred)
+class Classify:
+    def __init__(self, inp=None):
+        self.model = joblib.load(self.get_path('model.joblib'))
+        self.vectorizer = joblib.load(self.get_path('vectorizer.joblib'))
+        self.inp = inp
     
-    if y_pred[0] == 1:
-        print('This email is classified as Spam')
-    else:
-        print('This email is classified as Not Spam')
+    def get_path(self, name):
+        return os.path.join(settings.BASE_DIR, 'spam', 'ml_models', name)
+    
+    def get_input(self):
+        if self.inp is None:
+            inp = input('Email to check: ')
+        tokenized_input = set(word for word in word_tokenize(inp)
+                if any(c.isalpha() for c in word))
+        return self.vectorizer.transform([tokenized_input])
 
+    def check_spam(self):
+        X_pred = self.get_input(self.vectorizer)
+        y_pred = self.model.predict(X_pred)
+
+        if y_pred[0] == 1:
+            return 'spam'
+        else:
+            return 'notspam'
+        
 def analyzer_function(x):
     return x
 
-def load_models():
-    model = joblib.load(model_name)
-    vectorizer = joblib.load(vectorizer_name)
-    return model, vectorizer
+def index(request):
+    return render(request, "spam/index.html")
 
-def get_input(vectorizer):
-    inp = input('Email to check: ')
-    tokenized_input = sentence_tokenizer(inp)
-    X_pred = vectorizer.transform([tokenized_input])
-    return X_pred
-
-def sentence_tokenizer(sentence):
-    return set(word for word in word_tokenize(sentence)
-               if any(c.isalpha() for c in word))
-
-if __name__ == '__main__':
-    main()
