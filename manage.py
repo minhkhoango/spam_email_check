4708ea2c9bb999ec68b210a04630f09e4458bb66
minhkhoango <ngominhkhoa2006@gmail.com>
2025-03-03 07:10:00 -0500

    done

--- manage.py
+++ manage.py
@@ -17,6 +17,8 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
+def analyzer_function(x):
+    return x
 
 if __name__ == '__main__':
     main()
--- spam/views.py
+++ spam/views.py
@@ -17,13 +17,13 @@ class Classify:
     
     def get_input(self):
         if self.inp is None:
-            inp = input('Email to check: ')
-        tokenized_input = set(word for word in word_tokenize(inp)
+            return None
+        tokenized_input = set(word for word in word_tokenize(self.inp)
                 if any(c.isalpha() for c in word))
         return self.vectorizer.transform([tokenized_input])
 
     def check_spam(self):
-        X_pred = self.get_input(self.vectorizer)
+        X_pred = self.get_input()
         y_pred = self.model.predict(X_pred)
 
         if y_pred[0] == 1:
@@ -35,5 +35,18 @@ def analyzer_function(x):
     return x
 
 def index(request):
-    return render(request, "spam/index.html")
+    if request.method == 'POST':
+        email_content = request.POST['email'].strip()
+        if not email_content:
+            return render(request, 'spam/index.html', {'spam_result': 'No input provded'})
+        
+        classifier = Classify(email_content)
+        spam_result = classifier.check_spam()
+        return render(request, 'spam/index.html', {
+            'spam_result': spam_result,
+        })
+        
+    return render(request, 'spam/index.html')
 
+
+    
